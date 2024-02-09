90092c17e589a24c01f48efb6f068be0a1956b61
Steve <stevenjdigregorio@gmail.com>
2024-02-09 14:23:01 -0700

    moved all into main

--- blink.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin
-from utime import sleep
-
-onBoardLED = Pin("LED", Pin.OUT)
-
-def blink():
-    onBoardLED.toggle()
-    sleep(1)
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from ota_updater import update_ota
-from blink import blink
+from machine import Pin
+from utime import sleep
 
+def blink():
+    onBoardLED.toggle()
+    sleep(1)
+# ––––––––––––––––––––––––––
 otaUpdatePin = Pin(5, Pin.IN, Pin.PULL_UP)
+onBoardLED = Pin("LED", Pin.OUT)
 
 while True:
     if not otaUpdatePin.value():
         update_ota()
     blink()
+
