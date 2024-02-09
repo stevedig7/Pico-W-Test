d485b7222f92d57cae33a9860fe3cc286de76a4b
Steve <stevenjdigregorio@gmail.com>
2024-02-09 10:53:23 -0700

    Initial Upload

---
+++ ota_updater.py
@@ -0,0 +1,6 @@
+from ota import OTAUpdater 
+from WIFI_CONFIG import SSID, PASSWORD
+
+firmware_url = "https://raw.githubusercontent.com/kevinmcaleer/ota_test/"
+ota_updater = 0TAUpdater (SSID, PASSWORD, firmware_url, "main.py")
+ota_updater.download_and_install_update_if_available()
