2210cee25db7088fe0ac8884b85360b25a52d544
Steve <stevenjdigregorio@gmail.com>
2024-02-09 15:41:35 -0700

    0.1 s

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utime import sleep
 def blink():
     print('blink')
     onBoardLED.toggle()
-    sleep(1)
+    sleep(0.1)
 # ––––––––––––––––––––––––––
 otaUpdatePin = Pin(5, Pin.IN, Pin.PULL_UP)
 onBoardLED = Pin("LED", Pin.OUT)
