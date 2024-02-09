8889673345e1426d3631326f6124c3625f69b0ed
Steve <stevenjdigregorio@gmail.com>
2024-02-09 15:03:22 -0700

    removed version

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
+from ota_updater import update_ota
 from machine import Pin
 from utime import sleep
-from ota_updater import update_ota
-
-def ota():
-    onBoardLED.value(1)
-    update_ota()
 
 def blink():
     onBoardLED.toggle()
-    sleep(0.1)
-
-# –––––––––––––––––––––––––––––––––––––
-onBoardLED = Pin("LED", Pin.OUT)
+    sleep(1)
+# ––––––––––––––––––––––––––
 otaUpdatePin = Pin(5, Pin.IN, Pin.PULL_UP)
+onBoardLED = Pin("LED", Pin.OUT)
 
 while True:
-    if not otaUpdatePin.value(): ota()
+    if not otaUpdatePin.value():
+        onBoardLED.value(1)
+        update_ota()
     blink()
