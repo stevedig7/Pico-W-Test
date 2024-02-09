671efc72827c997d67e36a8116c3aca031af651f
stevedig7 <49043186+stevedig7@users.noreply.github.com>
2024-02-09 12:58:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-## Code 2
-# from machine import Pin
-# from utime import sleep
 
-# pin = Pin("LED", Pin.OUT)
-
-# print("LED starts flashing...")
-# while True:
-#     try:
-#         pin.toggle()
-#         sleep(1) # sleep 1sec
-#     except KeyboardInterrupt:
-#         break
-# pin.off()
-# print("Finished.")
 print("hi")
