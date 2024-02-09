0d9a1cc4ded400bfd09824ec51f34d30efcc4559
Steve <stevenjdigregorio@gmail.com>
2024-02-09 14:25:38 -0700

    back to working code

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from ota_updater import update_ota
 from machine import Pin
 from utime import sleep
+from ota_updater import update_ota
 
-def blink():
-    onBoardLED.toggle()
-    sleep(1)
-# ––––––––––––––––––––––––––
+pin = Pin("LED", Pin.OUT)
 otaUpdatePin = Pin(5, Pin.IN, Pin.PULL_UP)
-onBoardLED = Pin("LED", Pin.OUT)
 
+print("LED starts flashing...")
 while True:
-    if not otaUpdatePin.value():
-        update_ota()
-    blink()
+    pin.toggle()
+    sleep(0.1)
 
+    if not otaUpdatePin.value():
+        print('Updating code from GitHub')
+        update_ota()
